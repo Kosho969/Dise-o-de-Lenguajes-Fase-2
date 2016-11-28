6cc1b7a77502165f1179f1861c6265d6b6563f65
Kosho969 <pablo.koch26@gmail.com>
2016-11-28 09:40:15 -0600

    Generador diferente.

--- src/Generator/AnalizadorLexico.java
+++ src/Generator/AnalizadorLexico.java
@@ -12,27 +12,36 @@ import java.io.File;
 import javax.swing.JFileChooser;
 public class AnalizadorLexico
 {
+    static ArrayList<String> WhiteSpaceAL = new ArrayList<String>(Arrays.asList("\n","\r","\t"," "));
+
+
+    static boolean[] EKTokenAL = {true, false, false, false, true};
+
+   static ArrayList<String> KeyRegexAL = new ArrayList<String>(Arrays.asList("«bÇoÇoÇlÇeÇaÇn»","«bÇyÇtÇe»","«cÇhÇaÇr»","«cÇlÇaÇsÇs»","«dÇoÇuÇbÇlÇe»","«fÇaÇlÇsÇe»","«fÇiÇnÇaÇl»","«fÇlÇoÇaÇt»","«iÇnÇt»","«lÇoÇnÇg»","«nÇeÇw»","«nÇuÇlÇl»","«sÇhÇoÇrÇt»","«sÇtÇaÇtÇiÇc»","«sÇuÇpÇeÇr»","«tÇhÇiÇs»","«tÇrÇuÇe»","«vÇoÇiÇd»","«:»","«,»","«-Ç-»","«.»","«+Ç+»","«{»","«[»","«(»","«-»","«!»","«+»","«}»","«]»","«)»"));
+
+    static ArrayList<String> KeyIdsAL = new ArrayList<String>(Arrays.asList("boolean","byte","char","class","double","false","final","float","int","long","new","null","short","static","super","this","true","void","colon","comma","dec","dot","inc","lbrace","lbrack","lpar","minus","not","plus","rbrace","rbrack","rpar"));
+
+    static ArrayList<String> TokenRegexAL = new ArrayList<String>(Arrays.asList("«aØbØcØdØeØfØgØhØiØjØkØlØmØnØoØpØqØrØsØtØuØvØwØxØyØzØAØBØCØDØEØFØGØHØIØJØKØLØMØNØOØPØQØRØSØTØUØVØWØXØYØZ»Ç««aØbØcØdØeØfØgØhØiØjØkØlØmØnØoØpØqØrØsØtØuØvØwØxØyØzØAØBØCØDØEØFØGØHØIØJØKØLØMØNØOØPØQØRØSØTØUØVØWØXØYØZ»Ø«0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9»»º","««0»Ø«1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9»Ç««0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9»»ºØ««0Çx»Ø«0ÇX»»Ç«0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9ØAØBØCØDØEØFØaØbØcØdØeØf»Ç««0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9ØAØBØCØDØEØFØaØbØcØdØeØf»»ºØ0Ç«0Ø1Ø2Ø3Ø4Ø5Ø6Ø7»Ç««0Ø1Ø2Ø3Ø4Ø5Ø6Ø7»»º»Ç«««l»Ø«L»»Ø~»","«.»Ç«0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9»Ç««0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9»»ºÇ««««e»Ø«E»»Ç«««+»Ø«-»»Ø~»Ç«0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9»Ç««0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9»»º»Ø~»Ç«««F»Ø«f»Ø«D»Ø«d»»Ø~»Ø«0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9»Ç««0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9»»ºÇ««.»Ç««0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9»»ºÇ««««e»Ø«E»»Ç«««+»Ø«-»»Ø~»Ç«0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9»Ç««0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9»»º»Ø~»Ç«««F»Ø«f»Ø«D»Ø«d»»Ø~»Ø««e»Ø«E»»Ç«««+»Ø«-»»Ø~»Ç«0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9»Ç««0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9»»ºÇ«««F»Ø«f»Ø«D»Ø«d»»Ø~»Ø«F»Ø«f»Ø«D»Ø«d»»","«'»Ç««aØbØcØdØeØfØgØhØiØjØkØlØmØnØoØpØqØrØsØtØuØvØwØxØyØzØAØBØCØDØEØFØGØHØIØJØKØLØMØNØOØPØQØRØSØTØUØVØWØXØYØZØ0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9Ø Ø!Ø%Ø^Ø&Ø=Ø?Ø>Ø<Ø:Ø.Ø,Ø(Ø[Ø{Ø)Ø$Ø]Ø}Ø-Ø+Ø\\Ø/Ø\"Ø*»Ø«\\»Ç««b»Ø«t»Ø«n»Ø«f»Ø«r»Ø«\"»Ø«\\Ç'»Ø«\\»Ø«u»Ç««u»»ºÇ«0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9ØAØBØCØDØEØFØaØbØcØdØeØf»Ç«0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9ØAØBØCØDØEØFØaØbØcØdØeØf»Ç«0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9ØAØBØCØDØEØFØaØbØcØdØeØf»Ç«0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9ØAØBØCØDØEØFØaØbØcØdØeØf»Ø«0Ø1Ø2Ø3»Ç«««0Ø1Ø2Ø3Ø4Ø5Ø6Ø7»»Ø~»Ç«««0Ø1Ø2Ø3Ø4Ø5Ø6Ø7»»Ø~»Ø«0Ø1Ø2Ø3Ø4Ø5Ø6Ø7»Ç«««0Ø1Ø2Ø3Ø4Ø5Ø6Ø7»»Ø~»»»Ç«'»","«\"»Ç««aØbØcØdØeØfØgØhØiØjØkØlØmØnØoØpØqØrØsØtØuØvØwØxØyØzØAØBØCØDØEØFØGØHØIØJØKØLØMØNØOØPØQØRØSØTØUØVØWØXØYØZØ0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9Ø Ø!Ø%Ø^Ø&Ø=Ø?Ø>Ø<Ø:Ø.Ø,Ø(Ø[Ø{Ø)Ø$Ø]Ø}Ø-Ø+Ø/Ø'Ø*Ø\\»Ø«\\»Ç««b»Ø«t»Ø«n»Ø«f»Ø«r»Ø«\\Ç'»Ø«\\»Ø«u»Ç««u»»ºÇ«0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9ØAØBØCØDØEØFØaØbØcØdØeØf»Ç«0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9ØAØBØCØDØEØFØaØbØcØdØeØf»Ç«0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9ØAØBØCØDØEØFØaØbØcØdØeØf»Ç«0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9ØAØBØCØDØEØFØaØbØcØdØeØf»Ø«0Ø1Ø2Ø3»Ç«««0Ø1Ø2Ø3Ø4Ø5Ø6Ø7»»Ø~»Ç«««0Ø1Ø2Ø3Ø4Ø5Ø6Ø7»»Ø~»Ø«0Ø1Ø2Ø3Ø4Ø5Ø6Ø7»Ç«««0Ø1Ø2Ø3Ø4Ø5Ø6Ø7»»Ø~»»»ºÇ«\"»"));
+
+    static ArrayList<String> TokenIdsAL = new ArrayList<String>(Arrays.asList("ident","intLit","floatLit","charLit","stringLit"));
+    static AutMaker maker = new AutMaker();
+
+    static ArrayList<AFD> KeyAuts = new ArrayList<AFD>();
+
+    static ArrayList<AFD> TokenAuts = new ArrayList<AFD>();
+
+    static ArrayList<String> currentToken = null;
+
+    static int currentTokenIndex = -1;
+
+    static ArrayList<ArrayList<String>> tokensAL = new ArrayList<ArrayList<String>>();
     public static void main(String[] args) throws FileNotFoundException, IOException
     {
-        ArrayList<String> WhiteSpaceAL = new ArrayList<String>(Arrays.asList("\n","\r"," ","    "));
- 
-        boolean[] EKTokenAL = {true, true, false};
- 
-        ArrayList<String> KeyRegexAL = new ArrayList<String>(Arrays.asList("«wÇhÇiÇlÇe»","«dÇo»","«;»","«=»"));
- 
-        ArrayList<String> KeyIdsAL = new ArrayList<String>(Arrays.asList("while","do","semicolon","equals"));
- 
-        ArrayList<String> TokenRegexAL = new ArrayList<String>(Arrays.asList("«aØbØcØdØeØfØgØhØiØjØkØlØmØnØoØpØqØrØsØtØuØvØwØxØyØzØAØBØCØDØEØFØGØHØIØJØKØLØMØNØOØPØQØRØSØTØUØVØWØXØYØZ»Ç««aØbØcØdØeØfØgØhØiØjØkØlØmØnØoØpØqØrØsØtØuØvØwØxØyØzØAØBØCØDØEØFØGØHØIØJØKØLØMØNØOØPØQØRØSØTØUØVØWØXØYØZ»Ø«0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9»»º","«0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9»Ç««0Ø1Ø2Ø3Ø4Ø5Ø6Ø7Ø8Ø9»»º"));
- 
-        ArrayList<String> TokenIdsAL = new ArrayList<String>(Arrays.asList("ident","number"));
- 
-        AutMaker maker = new AutMaker();
-        ArrayList<AFD> KeyAuts = new ArrayList<AFD>();
+
         for(String s : KeyRegexAL)
         {
             KeyAuts.add(maker.MakeAut(s));
         }
-        ArrayList<AFD> TokenAuts = new ArrayList<AFD>();
         for(String s : TokenRegexAL)
         {
             TokenAuts.add(maker.MakeAut(s));
@@ -47,8 +56,9 @@ public class AnalizadorLexico
         }
         JFileChooser fileChooser = new JFileChooser();
         int returnValue = fileChooser.showOpenDialog(null);
-        if (returnValue == JFileChooser.APPROVE_OPTION)
-        {
+        if (returnValue != JFileChooser.APPROVE_OPTION){
+            System.exit(0);
+        }
             File selectedFile = fileChooser.getSelectedFile();
             System.out.println(selectedFile.getName());
             BufferedReader in = new BufferedReader(new FileReader(selectedFile.getName()));
@@ -63,90 +73,113 @@ public class AnalizadorLexico
             }
             System.out.println(conc);
             String[] StrLine = conc.split("");
-            ArrayList<String> Tokens = new ArrayList<String>();
-            for(int i=0; i<StrLine.length; i++)
-            {
-                String tok = "";
-                int j=0;
-                if(WhiteSpaceAL.contains(StrLine[i]))
-                {
-                    //do nothing
-                }
-                else
-                {
-                    while(WhiteSpaceAL.contains(StrLine[i+j]) == false)
-                    {
-                        tok  = tok + StrLine[i+j];
-                        if(i+j+1 != StrLine.length)
-                        {
-                            j++;
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                    Tokens.add(tok);
-                }
-                i=i+j;
-            }
-            System.out.println(Tokens);
-            ArrayList<String> forRemoval = new ArrayList<String>();
-            for(int i=0; i<TokenAuts.size(); i++)
-            {
-                for(String s : Tokens)
-                {
-                    if(TokenAuts.get(i).simulacionPegao(s))
-                    {
-                        boolean found = false;
-                        if(EKTokenAL[i]==true)
-                        {
-                            for(int j=0; j<KeyAuts.size(); j++)
-                            {
-                                if(KeyAuts.get(j).simulacionPegao(s))
-                                {
-                                    System.out.println("<"+s+", "+KeyIdsAL.get(j)+">");
-                                    forRemoval.add(s);
-                                    found = true;
-                                }
-                            }
-                            if(found == false)
-                            {
-                                System.out.println("<"+s+", "+TokenIdsAL.get(i)+">");
-                                forRemoval.add(s);
-                            }
-                        }
-                        else
-                        {
-                            System.out.println("<"+s+", "+TokenIdsAL.get(i)+">");
-                            forRemoval.add(s);
-                        }
-                    }
-                }
-            }
-            Tokens.removeAll(forRemoval);
-            forRemoval.clear();
-            for(int k=0; k<KeyAuts.size(); k++)
-            {
-                for(String s : Tokens)
-                {
-                    if(KeyAuts.get(k).simulacionPegao(s))
-                    {
-                        System.out.println("<"+s+", "+KeyIdsAL.get(k)+">");
-            forRemoval.add(s);
-                    }
-                }
-            }
-            Tokens.removeAll(forRemoval);
-            if(Tokens.size() != 0)
-            {
-                System.out.println("Error, not recognized: " + Tokens);
-            }
-            ////Fin de lo agregado
-        }
-        else if(returnValue == JFileChooser.CANCEL_OPTION)
+        ArrayList<String> Lexemas = new ArrayList<String>();
+        for(int i=0; i<StrLine.length; i++)
         {
-            System.exit(0);
+                String tok = "";
+            int j=0;
+            if(WhiteSpaceAL.contains(StrLine[i]))
+            {
+                //do nothing
+            }
+            else
+            {
+                while(WhiteSpaceAL.contains(StrLine[i+j]) == false)
+                {
+                    tok = tok + StrLine[i+j];
+                    if(i+j+1 != StrLine.length)
+                    {
+                        j++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                Lexemas.add(tok);
+            }
+            i=i+j;
         }
+
+        ArrayList<String> forRemoval = new ArrayList<String>();
+
+        for (String lexema : Lexemas)
+        {
+            ArrayList<String> token = null;
+
+            // Si hace match con un token, excelente
+            int matchedTokenIndex = getMatchedTokenIndex(lexema);
+            if (-1 != matchedTokenIndex) {
+
+                // Si la gramática especifica EXCEPT KEYWORKDS
+                if (EKTokenAL[matchedTokenIndex] == true) {
+
+                    int matchedKeywordIndex = getMatchedKeywordIndex(lexema);
+                    if (-1 != matchedKeywordIndex) {
+
+
+
+                        token = buildToken(lexema, KeyIdsAL.get(matchedKeywordIndex));
+                    }
+
+                    if (-1 == matchedKeywordIndex) {
+                        token = buildToken(lexema, TokenIdsAL.get(matchedTokenIndex));
+                    }
+                } else {
+                    token = buildToken(lexema, TokenIdsAL.get(matchedTokenIndex));
+                }
+            }
+
+            if (null != token) {
+                tokensAL.add(token);
+
+                continue;
+            }
+
+            // Si hace match con un keyword, excelente
+            int matchedKeywordIndex = getMatchedKeywordIndex(lexema);
+            if (-1 != matchedKeywordIndex) {
+                token = buildToken(lexema, KeyIdsAL.get(matchedKeywordIndex));
+            }
+
+            if (null != token) {
+                tokensAL.add(token);
+
+                continue;
+            }
+
+            token = buildToken(lexema, "unrecognized");
+            tokensAL.add(token);
+        }
+        System.out.println(tokensAL);
+    }
+
+    private static int getMatchedTokenIndex(String lexema)
+    {
+        for (int i=0; i<TokenAuts.size(); i++)
+        {
+            if (TokenAuts.get(i).simulacionPegao(lexema)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private static int getMatchedKeywordIndex(String lexema)
+    {
+        for (int i=0; i<KeyAuts.size(); i++)
+        {
+            if (KeyAuts.get(i).simulacionPegao(lexema)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private static ArrayList<String> buildToken(String lexema, String tokenName)
+    {
+        return new ArrayList<String>(Arrays.asList(lexema, tokenName));
     }
 }
